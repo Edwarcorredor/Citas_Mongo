f83ceb43b273ed4f2e47f8105e6c29fc477beef6
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-07 15:39:25 -0500

    feat: :sparkles: Conexion con routers

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import pacientesRouter from './routes/pacientes.js';
+import medicosRouter from './routes/medicos.js';
+import citasRouter from './routes/citas.js';
+import { crearToken as loginFn } from './middlewares/jwt.js'
+import passportConfig from './middlewares/passportConfig.js';
+import authorization from './middlewares/authorization.js'
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.post("/login", loginFn)
+
+app.use(passportConfig.authenticate('bearer', { session: false }), authorization)
+app.use("/pacientes", pacientesRouter);
+app.use("/medicos", medicosRouter);
+app.use("/citas", citasRouter);
+
+
+app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on http://localhost:${process.env.PORT}`),
+);
